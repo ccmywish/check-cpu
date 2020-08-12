b804a2ebb65d8676dc1b082f67127efc9466f638
ccmywish <leslieranaa@qq.com>
2020-08-12 12:32:11 +0800

    add readme

--- auto.sh
+++ auto.sh
@@ -1,4 +1,4 @@
 rm ./check-cpu.tar.gz
 tar  -czf ./check-cpu.tar.gz ./
 rsync -avz -e "ssh -i ~/Downloads/aoran" ./check-cpu.tar.gz ubuntu@139.155.26.88:/home/ubuntu/myapp
-#sudo mv /home/ubuntu/myapp/wc.tar.gz /var/www/html
+#sudo mv /home/ubuntu/myapp/check-cpu.tar.gz /var/www/html
---
+++ readme.md
@@ -0,0 +1,1 @@
+check cpu usage using sinatra
